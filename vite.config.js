c5be2c96c8dfe7765b1f74d063bb1f08524edd99
Owenlrj98 <owenlrj98@gmail.com>
2024-08-16 01:02:48 +0800

    test 4

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: [
-        'react-bootstrap/Container'
+        'react-bootstrap/Container',
+        'react-bootstrap/Nav'
       ]
     }
   }
